4b414c871afa6b5d5c86682e50d304d88ebb9883
sriramaleti9238 <sri13042000@gmail.com>
2023-04-12 05:20:33 +0530

    config server

---
+++ src/test/java/com/config/G1ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
